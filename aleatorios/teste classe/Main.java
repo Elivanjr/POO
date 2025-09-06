4312f31a4923f8ae8141107226b95421e308171b
Elivan Junior <elivanvljr@gmail.com>
2025-09-06 15:46:43 -0300

    Update Main.java

--- aleatorios/teste classe/Main.java
+++ aleatorios/teste classe/Main.java
@@ -1,12 +1,11 @@
 public class Main{
 	public static void main(String args[]){
-		int n = 9;
 		Humano chapisco = new Humano("Chapisco", 21);
 				
-		for(int i = 0; i < 10; i++){
+		for(int i = 0; i <= 10; i++){
 			chapisco.dormir(i);
 			
-			if(i == 9){
+			if(i == 10){
 				chapisco.end();
 			}	
 		}
