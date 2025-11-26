2eb3f760710bec1f62dd3ecd655d9dd40dc238e7
Elivanjr <elivanvljr@gmail.com>
2025-11-26 10:37:13 -0300

    att

--- POO_Heranca/1/Empregado.java
+++ POO_Heranca/1/Empregado.java
@@ -1,20 +1,23 @@
 public class Empregado extends Pessoa {
+	//Atributos
 	protected int codigoSetor;
 	protected Double salarioBase;
 	protected Double imposto;
 	
-	
+	//Contrutor
 	public Empregado(String nome, String endereco, String telefone, int codigoSetor, Double salarioBase, Double imposto){
-		super(nome, endereco, telefone);
+		super(nome, endereco, telefone); //chama o construtor da Super Classe Pessoa;
 		this.codigoSetor = codigoSetor;
 		this.salarioBase = salarioBase;
 		this.imposto = imposto;
 	}
 	
+	//Métodos
 	public Double calculaSalario(){
 		return this.salarioBase - this.imposto;
 	}
 	
+	//Getters & Setters
 	public int getCodigoSetor() {
 		return codigoSetor;
 	}
--- POO_Heranca/1/Fornecedor.java
+++ POO_Heranca/1/Fornecedor.java
@@ -1,17 +1,21 @@
 public class Fornecedor extends Pessoa {
+	//Atributos
 	private Double credito;
 	private Double divida;
 	
+	//Construtor
 	public Fornecedor(String nome, String endereco, String telefone, Double credito, Double divida){
-		super(nome, endereco, telefone);
+		super(nome, endereco, telefone); //chama o construtor da Super Classe Pessoa;
 		this.credito = credito;
 		this.divida = divida;
 	}
 	
+	//Métodos
 	public Double obterSaldo(){
 		return this.credito - this.divida;
 	}
 	
+	//Getters & Setters;
 	public Double getCredito(){
 		return credito;
 	}
@@ -22,5 +26,9 @@ public class Fornecedor extends Pessoa {
 	
 	public Double getDivida(){
 		return divida;
+	}
+	
+	public void setDivida(Double divida){
+		this.divida = divida;
 	}
 }
